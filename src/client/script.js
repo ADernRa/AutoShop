6a34ec19838e0fc878495d9f27378467dcf69413
Adern <magmed145@gmail.com>
2026-09-22 20:54:38 +0300

    feat(client.login): Створив панель для авторизації користувача на сайті

--- src/client/script.js
+++ src/client/script.js
@@ -148,6 +148,7 @@ function closeCart() {
     document.body.style.overflow = ''; 
 }
 
+
 if (cartLink) {
     cartLink.addEventListener('click', openCart);
 }
@@ -156,6 +157,61 @@ if (closeBtn) {
     closeBtn.addEventListener('click', closeCart);
 }
 
+const eye_button = document.querySelector('.eye')
+eye_button.addEventListener('click', (event) => {
+    const passwor_input = document.querySelector('.password')
+    const open_eye = document.querySelector('.open-eye')
+    const close_eye = document.querySelector('.close-eye')
+    if(eye_button.classList.contains('close')){
+        eye_button.classList.remove('close');
+        eye_button.classList.add('open');
+        passwor_input.setAttribute('type', 'text');
+        close_eye.style.display = 'none';
+        open_eye.style.display = 'flex';
+    }
+    else {
+        eye_button.classList.remove('open');
+        eye_button.classList.add('close');
+        passwor_input.setAttribute('type', 'password');
+        close_eye.style.display = 'flex';
+        open_eye.style.display = 'none'; 
+    }
+});
+
+const cart_login = document.querySelector('.cart-login')
+function closeLogin(){
+    cart_login.classList.remove('open');
+    cartOverlay.classList.remove('show');
+    document.body.style.overflow = ''; 
+}
+
+function openLogin(e){
+    e.preventDefault(); 
+    cart_login.classList.add('open');
+    cartOverlay.classList.add('show');
+    document.body.style.overflow = ''; 
+}
+
+const close_btn_login = document.querySelector('.close-btn-login')
+const open_btn_login = document.querySelector('.user-link')
+
+if (close_btn_login){
+    close_btn_login.addEventListener('click', closeLogin);
+}
+
+if (open_btn_login){
+    open_btn_login.addEventListener('click', openLogin);
+}
+
+function closeOverlay() {
+    if(cartSidebar.classList.contains('open')){
+        closeCart();
+    }
+    if(cart_login.classList.contains('open')){
+        closeLogin()
+    }
+}
+
 if (cartOverlay) {
-     cartOverlay.addEventListener('click', closeCart);
+    cartOverlay.addEventListener('click', closeOverlay);
 }
